53ad58830ce1a106c047454bf68cefa6a9c0b02a
ferhadme <frhdmehdiyev@gmail.com>
2022-03-14 13:46:56 +0400

    initial commit

---
+++ src/main/java/com/ferhad/unitech/UnitechApplication.java
@@ -0,0 +1,13 @@
+package com.ferhad.unitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitechApplication.class, args);
+	}
+
+}
